3ab40e407888416b1a231d8af141d3f6f7b68be4
Becki Wordsworth <becki@hiotlabs.com>
2022-12-25 19:55:39 +0000

    Adding day four answer

---
+++ Day Four/dayfour.js
@@ -0,0 +1,50 @@
+const { readFileSync }  = require('fs');
+
+const pairs = readFileSync('dayfour.txt', { encoding: 'utf-8' })
+.split(/\r?\n/);
+
+function part1() {
+
+let fullyContainedPairs = 0;
+
+for (const pair of pairs) {
+  if (pair === '') break;
+
+  const [section1, section2] = pair
+    .split(',')
+    .map((section) => section.split('-').map((range) => parseInt(range)));
+
+  if (
+    (section1[0] >= section2[0] && section1[1] <= section2[1]) ||
+    (section2[0] >= section1[0] && section2[1] <= section1[1])
+  )
+    fullyContainedPairs++;
+}
+
+console.log(fullyContainedPairs);
+
+}
+
+function part2() {
+
+let overlappingPairs = 0;
+
+for (const pair of pairs) {
+  if (pair === '') break;
+
+  const [section1, section2] = pair
+    .split(',')
+    .map((section) => section.split('-').map((range) => parseInt(range)));
+
+  if (
+    (section1[0] <= section2[0] && section1[1] >= section2[0]) ||
+    (section2[0] <= section1[0] && section2[1] >= section1[0])
+  )
+    overlappingPairs++;
+}
+
+console.log(overlappingPairs);
+}
+
+part1();
+part2();
